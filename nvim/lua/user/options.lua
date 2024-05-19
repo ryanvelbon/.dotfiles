67a7588355d95235972e1d3418fdf6d9e8813337
Ryan Vel Bon <30857718+ryanvelbon@user.noreply.github.com>
2024-05-19 08:44:52 +0200

    adds Neovim settings

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,1 +1,48 @@
-print("this is the Options file")
+---------------------------------------
+-- author : Jess Archer
+-- url    : https://github.com/laracasts/laracasts-dotfiles/blob/main/nvim/lua/user/options.lua
+-- video  : https://laracasts.com/series/neovim-as-a-php-ide/episodes/9
+---------------------------------------
+
+vim.opt.expandtab = true -- not working
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wildmode = 'longest:full,full'
+
+vim.opt.title = true
+--vim.opt.mouse = 'a' -- warning: this seems to disable right click menu
+
+vim.opt.termguicolors = true
+
+vim.opt.spell = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.list = true -- enable the below listchars
+vim.opt.listchars = { tab = '▸ ', trail = '·' }
+vim.opt.fillchars:append({ eob = ' ' }) -- remove the ~ from end of buffer
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.clipboard = 'unnamedplus' -- use system clipboard
+
+vim.opt.confirm = true -- ask for confirmation instead of erroring
+
+vim.opt.undofile = true -- persist undos
+vim.opt.backup = true -- automatically save a backup file
+vim.opt.backupdir:remove('.') -- keep backups out of the current directory
+
